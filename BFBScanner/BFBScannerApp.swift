0ec03c456dc90da44de85d1902b635ac46bf0b47
Faten Mahmoudi <faten.mahmoudi@vo2-group.com>
2023-10-16 19:05:57 +0200

    init commit

---
+++ BFBScanner/BFBScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BFBScannerApp.swift
+//  BFBScanner
+//
+//  Created by Faten Mahmoudi on 16/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BFBScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
